2950504caa9ce3375195ae6a1e095f02b95a30e2
Shilin Zhao <slzhao@sibs.ac.cn>
2017-04-15 06:30:10 +0000

    bugfix
    From: slzhao <zhaoshilin@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/RnaSeqSampleSize@128741 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/convertIdOneToOne.R
+++ R/convertIdOneToOne.R
@@ -12,8 +12,8 @@
 ##' @importFrom biomaRt getBM useMart
 ##' @export
 ##' @examples x<-c("Q04837","P0C0L4","P0C0L5","O75379","Q13068","A2MYD1","P60709","P30462","P30475","P30479")
-##' convertIdOneToOne(x,filters="uniprot_swissprot",verbose=TRUE)
-convertIdOneToOne<-function(x,dataset="hsapiens_gene_ensembl",filters="uniprot_swissprot",attributes =c(filters,"entrezgene"),verbose=FALSE) {
+##' convertIdOneToOne(x,filters="uniprotswissprot",verbose=TRUE)
+convertIdOneToOne<-function(x,dataset="hsapiens_gene_ensembl",filters="uniprotswissprot",attributes =c(filters,"entrezgene"),verbose=FALSE) {
 	if (verbose) {
 		cat("Now conectting with ensembl. Internet acess is needed and it may use 30 seconds.\n")
 		flush.console()
@@ -22,9 +22,12 @@ convertIdOneToOne<-function(x,dataset="hsapiens_gene_ensembl",filters="uniprot_s
 	ensembl = useMart("ensembl",dataset=dataset)
 	newIdTable<-getBM(attributes =attributes,filters=filters,values=x,mart = ensembl)
 	newIdTable<-newIdTable[which(newIdTable[,1]!="" & newIdTable[,2]!=""),]
+	if (verbose & any(table(newIdTable[,1])>1)) {
+		cat(paste0(length(which(table(newIdTable[,1])>1))," Ids can be converted to more than one ",attributes[2],", will use the first one.\n"))
+	}
 	result<-newIdTable[match(x, newIdTable[,1]),2]
 	if (verbose & any(is.na(result))) {
-		cat(paste0(length(which(is.na(result)))," Ids cant't be converted to ",attributes[2],", will be set as NA\n"))
+		cat(paste0(length(which(is.na(result)))," Ids cant't be converted to ",attributes[2],", will be set as NA.\n"))
 	}
 	names(result)<-x
 	return(result)
