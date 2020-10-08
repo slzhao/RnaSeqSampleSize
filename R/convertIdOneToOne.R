ea31e14c90420e50da4ed4ba16a456464a192c3d
Shilin Zhao <zhaoshilin@gmail.com>
2020-10-08 15:46:38 -0500

    updates to work with R 4.0

--- R/convertIdOneToOne.R
+++ R/convertIdOneToOne.R
@@ -11,16 +11,16 @@
 ##' @return A converted ID character with the same order of parameter x.
 ##' @importFrom biomaRt getBM useMart
 ##' @export
-##' @examples x<-c("Q04837","P0C0L4","P0C0L5","O75379","Q13068","A2MYD1","P60709","P30462","P30475","P30479")
+##' @examples x<-c("Q04837","P0C0L4","P0C0L5","O75379","Q13068","A2MYD1")
 ##' convertIdOneToOne(x,filters="uniprotswissprot",verbose=TRUE)
-convertIdOneToOne<-function(x,dataset="hsapiens_gene_ensembl",filters="uniprotswissprot",attributes =c(filters,"entrezgene"),verbose=FALSE) {
+convertIdOneToOne<-function(x,dataset="hsapiens_gene_ensembl",filters="uniprotswissprot",attributes =c(filters,"entrezgene_id"),verbose=FALSE) {
 	if (verbose) {
 		cat("Now conectting with ensembl. Internet acess is needed and it may use 30 seconds.\n")
-		flush.console()
+	  utils::flush.console()
 	}
 	
-	ensembl = useMart("ensembl",dataset=dataset)
-	newIdTable<-getBM(attributes =attributes,filters=filters,values=x,mart = ensembl)
+	ensembl = biomaRt::useMart("ensembl",dataset=dataset)
+	newIdTable<-biomaRt::getBM(attributes =attributes,filters=filters,values=x,mart = ensembl)
 	newIdTable<-newIdTable[which(newIdTable[,1]!="" & newIdTable[,2]!=""),]
 	if (verbose & any(table(newIdTable[,1])>1)) {
 		cat(paste0(length(which(table(newIdTable[,1])>1))," Ids can be converted to more than one ",attributes[2],", will use the first one.\n"))
